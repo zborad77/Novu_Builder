d5cf4d42c827c09cb35ebb06913555c63d002d0f
zborad77 <zbozinekradek@gmail.com>
2026-03-10 01:09:57 +0100

    update aplikace

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{padding:40}}>
+      <h1>NOVU Builder</h1>
+      <p>Aplikace pro řemeslníky a stavební firmy</p>
+    </div>
+  );
+}
+
+export default App;
